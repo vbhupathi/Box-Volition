3dff300fdaac142ef0ac38220c88eb438590c277
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-02-15 10:27:34 -0500

    Invite a user to join the enterprise

--- BoxUserInvite/Program.cs
+++ BoxUserInvite/Program.cs
@@ -14,24 +14,19 @@ namespace BoxUserInvite
         static void Main(string[] args)
         {
             string UserEmail = "sampleemail@abc.edu";        
-            string UserId = "0123456789";
             var appConfig = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
             var configJson = appConfig["BoxConfigJson"];
             System.Console.WriteLine($"Creating a Box Admin Client");
-
-            var config = BoxConfig.CreateFromJsonString(configJson);
+            var config = BoxConfig.CreateFromJsonString(configJson);        
             var auth = new BoxJWTAuth(config);
             var adminToken = auth.AdminToken();
             var boxClient = auth.AdminClient(adminToken);
             System.Console.WriteLine($"Created a Box Admin Client");
-
             var inviteEmail = new BoxActionableByRequest() {Login = UserEmail};
-            var inviteId = new BoxRequestEntity() { Id = UserId, Type = new BoxType()};
+            var inviteId = new BoxRequestEntity() { Id = config.EnterpriseId, Type = new BoxType()};
             System.Console.WriteLine($"Inviting User to enterprise account");
             boxClient.UsersManager.InviteUserToEnterpriseAsync(new BoxUserInviteRequest(){ Enterprise = inviteId, ActionableBy = inviteEmail});
             System.Console.WriteLine($"User invited to enterprise account");
-
-
         }
     }
 }
