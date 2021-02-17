869eb19ec54fadd63c0f2ff20cf336182c21cab4
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2021-02-16 22:38:42 -0500

    Extract Method

--- BoxUserInvite/Program.cs
+++ BoxUserInvite/Program.cs
@@ -13,20 +13,25 @@ namespace BoxUserInvite
 
         static void Main(string[] args)
         {
-            string UserEmail = "sampleemail@abc.edu";        
+            string UserEmail = "sampleemail@abc.edu";
             var appConfig = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-            var configJson = appConfig["BoxConfigJson"];
-            System.Console.WriteLine($"Creating a Box Admin Client");
-            var config = BoxConfig.CreateFromJsonString(configJson);        
+            var configJson = appConfig["BoxConfigJson"];            
+            Console.WriteLine($"Creating a Box Admin Client");
+            var config = BoxConfig.CreateFromJsonString(configJson);
             var auth = new BoxJWTAuth(config);
             var adminToken = auth.AdminToken();
             var boxClient = auth.AdminClient(adminToken);
-            System.Console.WriteLine($"Created a Box Admin Client");
-            var inviteEmail = new BoxActionableByRequest() {Login = UserEmail};
-            var inviteId = new BoxRequestEntity() { Id = config.EnterpriseId, Type = new BoxType()};
-            System.Console.WriteLine($"Inviting User to enterprise account");
-            boxClient.UsersManager.InviteUserToEnterpriseAsync(new BoxUserInviteRequest(){ Enterprise = inviteId, ActionableBy = inviteEmail});
-            System.Console.WriteLine($"User invited to enterprise account");
+            Console.WriteLine($"Created a Box Admin Client");
+            InviteUser(UserEmail, config, boxClient);
+        }
+
+        private static void InviteUser(string UserEmail, IBoxConfig config, BoxClient boxClient)
+        {
+            Console.WriteLine($"🔰 Initiate User invite...");
+            var inviteEmail = new BoxActionableByRequest() { Login = UserEmail };
+            var inviteId = new BoxRequestEntity() { Id = config.EnterpriseId, Type = new BoxType() };
+            boxClient.UsersManager.InviteUserToEnterpriseAsync(new BoxUserInviteRequest() { Enterprise = inviteId, ActionableBy = inviteEmail });
+            Console.WriteLine($"🏁 User invited to enterprise account");
         }
     }
 }
